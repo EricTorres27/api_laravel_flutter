08e818264a201a5cd4b8a1e4863083569b37af4b
EricTorres27 <Ilovethisgame17*>
2020-11-19 19:25:22 -0600

    se configuro para next advisor

---
+++ lib/view/perfil.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PerfilWidget extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      child: Text("Perfil"),
+      );
+  }
+}
